6fea5e12f6476cb1411a8624b456512676034b3e
Дмитрий <eryollerext@mail.ru>
2024-05-01 22:17:09 +0300

    JD-6: Изменил структуру проекта. Удалил lombok

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,13 +1,7 @@
 package model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 import service.Status;
 
-@Setter
-@Getter
-@ToString
 public class Task {
 
     private int id;
@@ -20,5 +14,47 @@ public class Task {
     }
 
     public Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
